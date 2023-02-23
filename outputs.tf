9b28fe2f28555402b93cbf6e83f5dd6949931399
Emmanuel Bergerat <45499891+gopher194@users.noreply.github.com>
2023-02-23 13:37:00 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,11 +7,11 @@ output "name" {
   description = "The generated name of the resource by the module."
 }
 output "location" {
-  value       = local.location_names[var.region_code]
+  value       = local.location_name
   description = "Location name compliant with `Azure Regions`' names. The list can be fetched with `az account list-locations --query '[].name'`."
 }
 output "location_display_name" {
-  value       = local.location_display_names[var.region_code]
+  value       = local.location_display_name
   description = "Location display name."
 }
 output "tags" {
